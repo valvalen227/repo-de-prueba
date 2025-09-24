b0df401aca1544d6c196ca927e571dfd7c61e5ad
-Semana4_Ordenamientos. <valentina_neirago@fet.edu.co>
2025-09-24 09:23:52 -0500

    agregar lista de nue....

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -8,6 +8,46 @@ public class Demo1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
+        // Generar y mostrar la lista de 100 números aleatorios
+        int[] numeros = generarNumerosAleatorios(100);
+        System.out.println("Lista original:");
+        mostrarArray(numeros);
+        // Ordenar usando burbuja
+        burbuja(numeros);
+        System.out.println("Lista ordenada:");
+        mostrarArray(numeros);
+    }
+
+    // Método para generar un array de n números aleatorios entre 0 y 999
+    public static int[] generarNumerosAleatorios(int n) {
+        int[] arr = new int[n];
+        java.util.Random rand = new java.util.Random();
+        for (int i = 0; i < n; i++) {
+            arr[i] = rand.nextInt(1000);
+        }
+        return arr;
+    }
+
+    // Método para ordenar un array usando el algoritmo burbuja
+    public static void burbuja(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Método para mostrar el array
+    public static void mostrarArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 
 }
